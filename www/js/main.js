3c5284a6114eef48c81cb3a4633d541bf169256f
Tun Yar Zar Toe <121626813+tunyarzartoe@users.noreply.github.com>
2026-08-09 18:29:44 +0900

    main.js を更新

--- www/js/main.js
+++ www/js/main.js
@@ -2,17 +2,14 @@
 
 // 要素の取得
 const ball = document.getElementById('ball');
-
 // ボールの移動量
 let deltaX = 0;  // x座標方向の移動量
 let deltaY = 0;  // y座標方向の移動量
-
 // deviceorientationイベントは，方角センサーから最新のデータが得られた場合に発生
 window.addEventListener('deviceorientation', e => {
     deltaX = e.gamma * 0.3;  // x座標方向の移動量（0.3倍して移動速度を調整）
     deltaY = e.beta * 0.3;  // y座標方向の移動量（0.3倍して移動速度を調整）
 });
-
 // 20ミリ秒ごとにボールの移動を描画を繰り返す
 setInterval(() => {
     const nowX = ball.offsetLeft;  // 現在の（親要素から見た相対的な）x座標
